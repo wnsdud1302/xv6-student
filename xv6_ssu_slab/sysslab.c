c714befe3a3be1109058338d6dc6290f151c838f
junyeong <wnsdud1302@naver.com>
2021-05-11 22:51:10 +0900

    6

--- xv6_ssu_slab/sysslab.c
+++ xv6_ssu_slab/sysslab.c
@@ -14,23 +14,23 @@
 int
 slabtest(void)
 {
-	//cprintf("__slabtest__\n");
+	cprintf("__slabtest__\n");
 
 	int x=TEST_ALLOC_MIN_SIZE;
 	char* t[NTEST];
 	int i;
 
-	//cprintf("Initial state ...\n");
+	cprintf("Initial state ...\n");
 	slabdump();
 
 	while (x <= TEST_ALLOC_MAX_SIZE) {
-		//cprintf("Allocate %dB memory ...\n", x-1);
+		cprintf("Allocate %dB memory ...\n", x-1);
 		for(i=0;i<NTEST;i++){
 			t[i] = (char *)kmalloc(x-1);
 		}
 		slabdump();
 
-		//cprintf("Free %dB memory ...\n", x-1);
+		cprintf("Free %dB memory ...\n", x-1);
 		for(i=0;i<NTEST;i++){
 			kmfree((char*)t[i], x-1);
 		}
